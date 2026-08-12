11eeccbc2e6cef0fe355290e4ea415845280112f
Mehmet Hasan <mehmet.hasan.bd@gmail.com>
2026-08-12 20:54:52 +0600

    statements

--- operator.py
+++ operator.py
@@ -17,8 +17,12 @@ print("total console solds of minecraft:",total_console_solds)
 print("average solds",total_console_solds/6)
 #price and solds
 solds_per_players=212000000
-player_per_sold=java_edition//bedrock_edition//pocket_edition+xbox_360+minecraft_dungeons+minecraft_legends
-print("total sold per person",sold_count)
-
-#floor division introduction
-sold_count=total
+player_per_sold=java_edition+bedrock_edition//pocket_edition+xbox_360+minecraft_dungeons+minecraft_legends
+print("total sold per person",player_per_sold)
+#calculator machine gun
+months=3
+years=10
+updating_the_game=months//3
+next_movie_release=years%10
+print("months for the upddate:",updating_the_game)
+print("years for the movie:",next_movie_release)
