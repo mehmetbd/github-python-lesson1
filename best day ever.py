e9c5dbb3ec6d91c48069d5ecf81f46156a56d0c8
Mehmet Hasan <mehmet.hasan.bd@gmail.com>
2026-08-14 21:00:27 +0600

    new statements

---
+++ best day ever.py
@@ -0,0 +1,25 @@
+#print and functions
+print("===Your Best Day===")
+print("hello i am an assistent answer 3 thing that you like to do in a weekend")
+day=input("what is the day:").strip().capitalize()
+gaming=input("what are you going to play:").strip().capitalize()
+going=input("where were you want to go now:").strip().lower()
+print()
+print(f"===today is {day}===")
+print("-"*35)
+#if,else,else,and,or,not statement
+if day in("friday"):
+    print("day is for staying in home freely and doing codingal class and doing aloha")
+elif day in("sunday,monday,tuesday,wednessday"):
+    print("today is your work day")
+elif day in("thursday"):
+    print("last day at school")
+elif day in("saturday"):
+    print("today is your free day do your homework play games for 1hour it will be fun today")
+else:
+    print("invalid syntext")
+if gaming =="forza horizon 5"and going =="Mcdonald's":
+    print("go to Mcdonald's to buy big mac and a fries and play forza horizon 5 for hours")
+elif gaming =="minecraft" or "mario kart 8 deluxe and mario kart world":
+    print("go to hypixel to play beddwars and if you have a switch and a switch 2 play mario kart games")
+print("planing complated thank you for talking")     
