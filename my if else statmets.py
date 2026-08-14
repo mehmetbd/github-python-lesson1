f06ae794c1ce9893f75651303487083e8899198d
Mehmet Hasan <mehmet.hasan.bd@gmail.com>
2026-08-14 19:38:36 +0600

    My  Q/A

---
+++ my if else statmets.py
@@ -0,0 +1,32 @@
+#1st Q/A
+game_choose=input("if ou are bored and wanted to play games which one would you play:")
+if game_choose=="minecraft":
+    print("you can play it for $30.00.")
+elif game_choose=="roblox":
+    print("dont play this stupid game")
+elif game_choose=="doom":
+    print("your favourate game is unsurprisingly older than you")
+elif game_choose=="GTA V":
+    print("GTA V thats a nice open world game")
+elif game_choose=="Wii SPORTS":
+    print("you are a pretty great nintenddo wii player")
+elif game_choose=="red dead redpition 2":
+    print("goodd game i like it")
+elif game_choose=="pokemon:RED/BLUE/YELLOW":
+    print("pokemon,here we go baby!")
+elif game_choose=="mario kart 8+mario kart 8 deluxe":
+    print("biggest mario kart fan lets go!")
+else:
+    print("just watch youtube if you want to ok")    
+#2nd Q/A
+job_choose=input("if you graduate and want a job which one will be:")
+if job_choose=="pizza delivery":
+    print("you gonna deliver pizza right")
+if job_choose=="tech company":
+    print("you now work at apple thats great")
+if job_choose=="streamer":
+    print("you now have to play forza horizon 5 for just 1hours anddd have to record it right")
+if job_choose=="youtuber":
+    print("now you use your camera to record from the outdoor")
+if job_choose=="NBA":
+    print("your now joining a NBA team later wow thats pretty expencive and great")        
