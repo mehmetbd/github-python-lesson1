c00ea4333df97838ed10f102fd7fef4ab644729e
Mehmet Hasan <mehmet.hasan.bd@gmail.com>
2026-08-21 19:31:35 +0600

    operator III

---
+++ operators 3.py
@@ -0,0 +1,18 @@
+#print
+print("===your gaming day===")
+print("enter 4 questions to make life boring to good again with franchise telling day today and console your playing")
+game=input("enter your favourate game:")
+franchise=input("enter your favourate franchise:")
+day=input("enter the day today:")
+console="enter the console your playing on:"
+#if else statement
+if game =="counter strike 2" and franchise=="counter strike" and day =="friday" or "saturay" and console == "Personal Computer" or "steam deck":
+    print("counter strike 2 and global offensive is the top 1 biggest games of all time in PC")
+if game =="minecraft" and franchise =="minecraft" and day =="friday"or"thursday" and console =="xbox 360"or"switch":
+    print("minecraft a 1 that stand out as top 1 great")
+if game =="super smash bros. ultimate" and franchise=="smash bros." and day=="saterday" or "sunday" and console=="switch":
+    print("super smash bros. ultimate your favourate nintendo franchise good")
+if game =="valorant" and franchise =="valorant" and day =="friday"or"sunday" and console=="PC":
+    print("valorant my favourate franchise nice")
+if game =="FC mobile" and franchise =="FC" and day =="friday" or "thursday" or "saturday" or "sunday" and console=="mobile":
+    print("FC mobile i played that")
