bc94990c6102d179092e710a327904f3f4d685fa
Mehmet Hasan <mehmet.hasan.bd@gmail.com>
2026-08-26 20:55:38 +0600

    my quiz

---
+++ operator4.py
@@ -0,0 +1,27 @@
+print("==============")
+print("favourate game")
+print("==============")
+print("Q1:pick a game")
+print("1-minecraft")
+print("2-geometry dash")
+choice=int(input("enter 1 or 2:"))
+if choice==1:
+    print("Q1.1:what is the rarest structure in minecraft")
+    print("1-woodlen mansions")
+    print("2-ancient city")
+
+    choice_2=int(input("enter 1 or 2:"))
+    if choice_2==1:
+        print("wrong answer")
+    else:    
+         print("correct")
+if choice==2:
+    print("Q1.1:what is the hardest level in geomerty dash")
+    print("1-society")
+    print("2-green bullet")
+
+    choice_2=int(input("enter 1 or 2:"))
+    if choice_2==1:
+        print("correct")
+    else:    
+         print("wrong")      
